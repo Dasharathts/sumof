2ad032355583885531d683eb9457f61b28214f95
Dasharathts <dasharathts16@gmail.com>
2021-09-24 15:43:40 +0530

    Initial commit

---
+++ CppMath/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter number 1"<<endl;
+    cin>>a;
+
+    cout<<"Enter number 2"<<endl;
+    cin>>b;
+
+    sum= a+b;
+    cout<<"The sum is: "<<sum<<endl;
+
+    return 0;
+
+}
